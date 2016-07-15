8c90d656cc7f2168779da9ea2be12616cd900696
Thanh Pham <minhthanh@me.com>
2016-07-15 18:08:00 +0800

    Sign in

--- TestRealm/ViewController.swift
+++ TestRealm/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        signIn()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func signIn() {
+        let parameters = [
+            "device": [
+                "app_version": "2.6.1",
+                "type": "IosDevice",
+                "name": "iPhone Simulator",
+                "device_token": "c920903a4555daecde37a0111ee8e38581c2ea7c0ff2ca69ea020c683c5ac5b9"
+            ],
+            "format": "json",
+            "user": [
+                "login": "thanh+patient@ring.md",
+                "password": "password"
+            ]
+        ]
+        Alamofire.request(.POST, "https://staging.ring.md/api/v5/public/tokens", parameters: parameters).responseJSON { response in
+            print("\(response.request?.URL): \(response)")
+        }
     }
-
-
 }
-
