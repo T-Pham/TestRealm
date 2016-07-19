620e3eb9cfd0dcc93205d818730046374c569127
Thanh Pham <minhthanh@me.com>
2016-07-19 13:15:29 +0800

    Move signIn to Requester

--- TestRealm/Requester.swift
+++ TestRealm/Requester.swift
@@ -21,6 +21,35 @@ struct Requester {
 
     static var userToken: String?
 
+    static func signIn(completionHandler: (Response<JSON> -> Void)? = nil) {
+        let parameters = [
+            "device": [
+                "app_version": "2.6.1",
+                "type": "IosDevice",
+                "name": "iPhone Simulator",
+                "device_token": "c920903a4555daecde37a0111ee8e38581c2ea7c0ff2ca69ea020c683c5ac5b9"
+            ],
+            "format": "json",
+            "user": [
+                "login": "thanh+patient@ring.md",
+                "password": "password"
+            ]
+        ]
+        Alamofire.request(.POST, "https://staging.ring.md/api/v5/public/tokens", parameters: parameters).responseJSON { response in
+            switch response.result {
+            case .Success(let value):
+                guard let json = value as? [String: AnyObject], userToken = json["authentication_token"] as? String else {
+                    completionHandler?(.Failure(nil, nil))
+                    return
+                }
+                Requester.userToken = userToken
+                completionHandler?(.Success(json))
+            case .Failure(let error):
+                completionHandler?(.Failure(error, nil))
+            }
+        }
+    }
+
     static func request<Type: DBObject>(method: Alamofire.Method, _ urlString: URLStringConvertible, parameters: [String: AnyObject]? = nil, completionHandler: (Response<Type> -> Void)? = nil) {
         var parameters = parameters ?? [String: AnyObject]()
         parameters["format"] = "json"
--- TestRealm/ViewController.swift
+++ TestRealm/ViewController.swift
@@ -17,26 +17,13 @@ class ViewController: UIViewController {
     }
 
     func signIn() {
-        let parameters = [
-            "device": [
-                "app_version": "2.6.1",
-                "type": "IosDevice",
-                "name": "iPhone Simulator",
-                "device_token": "c920903a4555daecde37a0111ee8e38581c2ea7c0ff2ca69ea020c683c5ac5b9"
-            ],
-            "format": "json",
-            "user": [
-                "login": "thanh+patient@ring.md",
-                "password": "password"
-            ]
-        ]
-        Alamofire.request(.POST, "https://staging.ring.md/api/v5/public/tokens", parameters: parameters).responseJSON { [weak self] response in
-            switch response.result {
-            case .Success(let value):
-                Requester.userToken = ((value as! NSDictionary)["authentication_token"] as! String)
+        Requester.signIn { [weak self] response in
+            switch response {
+            case .Success:
                 self?.test()
-            case .Failure(let error):
+            case .Failure(let error, let json):
                 print(error)
+                print(json)
             }
         }
     }
